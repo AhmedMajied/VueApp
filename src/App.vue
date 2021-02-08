8a9b89cdf00248197faf45e43dfe070080c3e097
Ahmed Majied <aabdelmeged70@yahoo.com>
2021-02-08 23:25:43 +0200

    initialization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- place it outside app div -->
+  <teleport to=".extra">
+    <Child :arr="arr" :title="title" @close="custom">
+      <!-- slot -->
+      <h1>I am a slot</h1>
+
+      <!-- named slot -->
+      <template v-slot:links>
+        <a href="#">link1</a>
+      </template>
+    </Child>
+  </teleport>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Child from "./components/child.vue";
 
 export default {
-  name: 'App',
+  // name: 'App',
   components: {
-    HelloWorld
+    Child,
+  },
+  data() {
+    return {
+      arr: [],
+      title: "Welcome to Your Vue.js App",
+    };
+  },
+  mounted(){
+    this.arr = ["name", "lool"]
+  },
+  methods: {
+    custom() {
+      alert("custom");
+    },
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="title" @click.self="customeEvent">{{title}}</div>
+
+  <ul>
+    <li v-for="item in arr" :key="item">{{item}}</li>
+  </ul>
+
+  <slot>place holder when there is no slot</slot>
+
+  <slot name="links"></slot>
+</template>
+
+<script>
+export default {
+  props: ["title", "arr"],
+  methods: {
+    customeEvent() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
